871ebf3a534b137d515dc14c61d892e2bbf9f0cd
manasamaddela <manasa5225@gmail.com>
2016-09-16 20:07:37 -0500

    Adding User-Display-Exercise

--- src/main/java/com/matc/persistence/UserData.java
+++ src/main/java/com/matc/persistence/UserData.java
@@ -7,16 +7,21 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.lang.Object;
+import java.text.*;
+import java.util.Date;
+
 
 /**
  * Access users in the user table.
  *
  * @author pwaite
  */
+@SuppressWarnings("ALL")
 public class UserData {
 
     private final Logger logger = Logger.getLogger(this.getClass());
@@ -125,7 +130,20 @@ public List<User> searchUserByLastName(String lastName) {
         user.setLastName(results.getString("last_name"));
         user.setFirstName(results.getString("first_name"));
         user.setUserid(results.getString("id"));
-        user.setUserAge(results.getString("date_of_birth"));
+
+        String dobString = results.getString("date_of_birth");
+
+        String[] dobParts = dobString.split("-");
+        int year = Integer.valueOf(dobParts[0]);
+        int month = Integer.valueOf(dobParts[1]);
+        int dateOfBirth = Integer.valueOf(dobParts[2]);
+
+        LocalDate birthday = LocalDate.of(year, month, dateOfBirth);
+        LocalDate today = LocalDate.now();
+        Period age = Period.between(birthday, today);
+
+        user.setUserAge(String.valueOf(age.getYears()));
+
         // TODO map the remaining fields
         return user;
     }
